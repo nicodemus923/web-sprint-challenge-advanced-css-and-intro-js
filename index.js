d3acd294971ba7d7168c3fccba274c6831f1dff9
Nic Leach <nicodemus923@gmail.com>
2020-10-12 20:34:18 -0600

    submitting what I have

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const artists = [
+const artists = [
     {
       "id": 0,
       "name": "Amedeo Modigliani",
@@ -210,12 +210,14 @@ Practice accessing data above by console.log-ing following items:
 (2) Bio of the third artist (2nd index) in the array */
 
 
-console.log(artists[0]);
+console.log(artists[0].name);
+console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+console.log(artists[8].name);
+artists[8].name = "Vincent Van Gogh";
+console.log(artists[8].name);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called `getArtistByIndex` that takes two arguments:
@@ -226,14 +228,37 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function printStrings(name){
+  return `my name is ${name}`
+}; 
+console.log(printStrings ("Jeff"));
+
+function printNumbers(num1, num2, num3){
+  return `the numbers are ${num1}, ${num2}, ${num3}`
+
+};
+// console.log(printNumbers (7, 19 , 11));
+
+// function getArtistName (artistsArray, id){
+//   return `the name is ${artistsArray[id].name}`
+
+// }
+// console.log(getArtistName(artists, 8));
+
+
+
+function getArtistByIndex(artistsArr, id) {
+  return `The artist at index ${artistsArr[id].id} is ${artistsArr[id].name}`
+  
+};
+console.log(getArtistByIndex(artists, 8));
+
+
 /*do I need to declare the array first?*/
-const getArtistByIndex = function(array, string;{
+/*const getArtistByIndex = function(array, string;{
   let newArr = [];
   for(let i = 0; i <array)
-})  
+//})  
 console.log("The artist at index {0} is {");
 
 
@@ -243,12 +268,12 @@ Create a function called get20s() that takes data as an argument and returns an 
 function get20s(/*Your Code Here*/){
   /*Your Code Here*/
 }
-for(let i = 0; i < artists.length; i++){
-  if ("years" = !< 1900 && !> 1999);
-    x = true;
-    else x = false;
-  console.log(artists[i].includes())  /*Need to gather and print the names that are true 
-}
+// for(let i = 0; i < artists.length; i++){
+//   if ("years" = !< 1900 && !> 1999);
+//     x = true;
+//     else x = false;
+//   console.log(artists[i].includes())  /*Need to gather and print the names that are true 
+// }
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -262,9 +287,9 @@ Create a function called `removeArtist` that takes two arguments:
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(artists.shift()) {
- console.log(value);
-}
+// function removeArtist(artists.shift()) {
+//  console.log(value);
+// }
    
 
 /**
@@ -281,9 +306,9 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(artists.push)){
-  console.log(value)
-  }
+// function addArtist(artists.push)){
+//   console.log(value)
+//   }
 /*then update the list*/
   
 
@@ -354,12 +379,12 @@ function randomize(/* Code here */){
   return 'bar';
 }
 /*Don't touch the code after this line! */
-export default{
-  foo,
-  getArtistByIndex,
-  get20s,
-  removeArtist,
-  addArtist,
-  lotsOfArt
-}
+// export default{
+//   foo,
+//   getArtistByIndex,
+//   get20s,
+//   removeArtist,
+//   addArtist,
+//   lotsOfArt
+// }
 /*TEST TEST TEST TEST TEST*/
